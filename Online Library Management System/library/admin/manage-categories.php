2589519c69c0536f11fe204d3e76e48ef2dc2301
adilah99 <73239944+adilah99@users.noreply.github.com>
2021-01-19 17:09:37 +0800

    Update manage-categories.php
    Add an 'id'  attribute to this <th> tag on line 115-119 (major). Also,replace this <i> tag by <em> on line 144(minor)

--- Online Library Management System/library/admin/manage-categories.php
+++ Online Library Management System/library/admin/manage-categories.php
@@ -111,12 +111,12 @@ header('location:manage-categories.php');
                                 <table class="table table-striped table-bordered table-hover" id="dataTables-example">
                                     <thead>
                                         <tr>
-                                            <th>#</th>
-                                            <th>Category</th>
-                                            <th>Status</th>
-                                            <th>Creation Date</th>
-                                            <th>Updation Date</th>
-                                            <th>Action</th>
+                                            <th id="#">#</th>
+                                            <th id="category">Category</th>
+                                            <th id="status">Status</th>
+                                            <th id="creationDate">Creation Date</th>
+                                            <th id="updationDate">Updation Date</th>
+                                            <th id="action">Action</th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -141,7 +141,7 @@ foreach($results as $result)
                                             <td class="center"><?php echo htmlentities($result->UpdationDate);?></td>
                                             <td class="center">
 
-                                            <a href="edit-category.php?catid=<?php echo htmlentities($result->id);?>"><button class="btn btn-primary"><i class="fa fa-edit "></i> Edit</button> 
+                                            <a href="edit-category.php?catid=<?php echo htmlentities($result->id);?>"><button class="btn btn-primary"><em class="fa fa-edit "></em> Edit</button> 
                                           <a href="manage-categories.php?del=<?php echo htmlentities($result->id);?>" onclick="return confirm('Are you sure you want to delete?');"" >  <button class="btn btn-danger"><i class="fa fa-pencil"></i> Delete</button>
                                             </td>
                                         </tr>
